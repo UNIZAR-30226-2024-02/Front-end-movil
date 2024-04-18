6b10ea3f44261d51ea5d41f9973a1f056906c1fd
Rubenmg27 <157393454+Rubenmg27@users.noreply.github.com>
2024-04-18 22:07:05 +0200

    Tienda arreglada
    Añadimos el skins_image.js para gestionar las imagenes en la tienda

---
+++ assets/Skins_image.js
@@ -0,0 +1,7 @@
+export const images=[
+    {index:'exampleSkin762', img:require("./OIG3.jpg")},
+    {index:'exampleSkin787878', img:require("./img-h7mkVoteflMRhJgU18vwF.jpeg")},
+    {index:'exampleSkin', img:require("./OIG2.jpg")},
+    {index:'exampleSkin2', img:require("./OIG1.jpg")},
+    {index:'exampleSkin3', img:require("./OIGL.jpg")}
+]
