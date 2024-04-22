60fb55ec29da204e6fc09ae49e90efccdd8280cb
Rubenmg27 <157393454+Rubenmg27@users.noreply.github.com>
2024-04-23 01:11:11 +0200

    Solucionado bugs skins

--- assets/Skins_image.js
+++ assets/Skins_image.js
@@ -1,8 +1,9 @@
 export const images=[
     {index:'exampleSkin762', img:require("./OIG3.jpg")},
     {index:'exampleSkin787878', img:require("./img-h7mkVoteflMRhJgU18vwF.jpeg")},
-    {index:'exampleSkin', img:require("./OIG2.jpg")},
-    {index:'exampleSkin2', img:require("./OIG1.jpg")},
-    {index:'exampleSkin3', img:require("./OIGL.jpg")},
-    {index:'ww2',img:require("./guerra.jpg")}
+    {index:'defaultSkin', img:require("./OIG2.jpg")},
+    {index:'defaultFichas', img:require("./OIG1.jpg")},
+    {index:'defaultMap', img:require("./OIGL.jpg")},
+    {index:'ww2',img:require("./guerra.jpg")},
+    {index:'lavaMap',img:require("./guerra.jpg")},
 ]
