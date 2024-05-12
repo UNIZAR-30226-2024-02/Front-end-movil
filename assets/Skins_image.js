8d4129d4c5cf370a75314e24c6c46a9905682d3e
Rubenmg27 <157393454+Rubenmg27@users.noreply.github.com>
2024-05-12 16:16:47 +0200

    Nuevas imagenes

--- assets/Skins_image.js
+++ assets/Skins_image.js
@@ -1,9 +1,11 @@
 export const images=[
-    {index:'exampleSkin762', img:require("./OIG3.jpg")},
-    {index:'exampleSkin787878', img:require("./img-h7mkVoteflMRhJgU18vwF.jpeg")},
+    {index:'Caballo', img:require("./OIG3.jpg")},
+    {index:'Soldado WW2', img:require("./img-h7mkVoteflMRhJgU18vwF.jpeg")},
     {index:'defaultSkin', img:require("./OIG2.jpg")},
     {index:'defaultFichas', img:require("./OIG1.jpg")},
     {index:'defaultMap', img:require("./OIGL.jpg")},
     {index:'ww2',img:require("./guerra.jpg")},
     {index:'lavaMap',img:require("./guerra.jpg")},
+    {index:'medieval',img:require("./medieval.png")},
+    {index:'templario',img:require("./templario.jpeg")},
 ]
